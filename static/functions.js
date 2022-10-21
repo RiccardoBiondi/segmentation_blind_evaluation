0edf785362850bc30356767247246441097a80cd
RiccardoBiondi <riccardo.biondi4@studio.unibo.it>
2022-10-21 14:16:34 +0200

    [ENH] Add the conre functionanlities to slide between the images in the specified folder.
    TODO: Add engine to save the results. Add Slide to show prev and next images

--- static/functions.js
+++ static/functions.js
@@ -14,21 +14,26 @@ const greenSrcs = [];
 var responses = {};
 
 // counter to keep track of the current image to display. 
-// TODO: if lower than 0 => get to the last slice 
-let currentSlideNumber = 0;
+let currentSlideIndex = 0;
 
 
-function loadFiles(filePickerId, dest){
+function loadFiles(filePickerId, dest, buttonId){
 
     let filePicker = document.getElementById(filePickerId);
+    let clickedButton = document.getElementById(buttonId)
     filePicker.click();
     filePicker.addEventListener('change', function(event){
-        let files = event.target.files;
+    let files = event.target.files;
         // append it to the global LINKS array
-        for (let i = 0; i < files.length; i++) {
-            dest.push(files[i].webkitRelativePath);
+    for (let i = 0; i < files.length; i++) {
+        dest.push(files[i].webkitRelativePath);
         };
-    }), false;
+    
+        clickedButton.innerHTML = "Uploaded " + files.length + " files from: " + files[0].webkitRelativePath.split('/')[0];
+    });
+
+    // change the text on the button to notify the folder selection
+    
 };
 
 
@@ -56,36 +61,82 @@ function setChoice(responseId){
     var filename = current.name;
 
     // Update the global dictionary
-    responses[filename + currentSlideNumber] = responseId;
-    currentSlideNumber += 1;
-    console.log(responses);
-
+    responses[filename + currentSlideIndex] = responseId;
     // Print message box
+    console.log(responses)
 }
+
 
 function confirmUploadDirectories(){
     
-    // check that you have select all the necessary files
 
-    let uploadPage = document.getElementById("uploadPage");
-    let evaluationPage = document.getElementById("evaluationPage");
+    // Ensure thet ypu have specified all the directories paths
+    if (imageSrcs.lengt == 0 || blueSrcs.length == 0 || greenSrcs.length == 0){
 
-    uploadPage.style.display = "none";
-    evaluationPage.style.display  = "initial";
+        alert("Pelase specify all the required directories");
+    }
+    // if all directories contains the same number of images you can proceed with the analysis
+    else if (imageSrcs.length == blueSrcs.length && imageSrcs.length == greenSrcs.length ){
+        let uploadPage = document.getElementById("uploadPage");
+        let evaluationPage = document.getElementById("evaluationPage");
+
+        uploadPage.style.display = "none";
+        evaluationPage.style.display  = "initial";
+
+        // show the first slide
+        showCurrentSlide(currentSlideIndex);
+    } else {
+
+        // if different number of images display an alert message
+
+        alert("All the folders must contains the same number of images");
+    }
+
+ 
 }
 
+
+function showImage(src, id){
+    /*Function to show an image specifying the src */ 
+    let image = document.getElementById(id);
+    image.src = './data/' + src;
+}
+
+function showCurrentSlide(n){
+    /* Function to show the current image to evaluate*/
+
+    // Force the wrap around
+    if (n > imageSrcs.length - 1) { currentSlideIndex = 0 }
+    if (n < 0) { currentSlideIndex = imageSrcs.length - 1 }
+
+    // now show the images
+
+    showImage(imageSrcs[currentSlideIndex], 'mainImage');
+    showImage(imageSrcs[currentSlideIndex], 'mainImageBis');
+    showImage(greenSrcs[currentSlideIndex], 'greenOverlay');
+    showImage(blueSrcs[currentSlideIndex], 'blueOverlay');
+
+}
+
+// Function to move torugh the images
+function moveImage(step){
+
+    currentSlideIndex += step;
+    showCurrentSlide(currentSlideIndex);
+}
 // Callbacks to upload the images
 
 
-document.getElementById("uploadImage").addEventListener('click', function (event) { loadFiles("imagePicker", imageSrcs)});
-document.getElementById("uploadBlue").addEventListener('click', function (event) { loadFiles("bluePicker", blueSrcs) });
-document.getElementById("uploadGreen").addEventListener('click', function (event) { loadFiles("greenPicker", greenSrcs)});
+document.getElementById("uploadImage").addEventListener('click', function () { loadFiles("imagePicker", imageSrcs, "uploadImage")});
+document.getElementById("uploadBlue").addEventListener('click', function () { loadFiles("bluePicker", blueSrcs, "uploadBlue") });
+document.getElementById("uploadGreen").addEventListener('click', function () { loadFiles("greenPicker", greenSrcs, "uploadGreen")});
 
 //document.getElementById("confirmButton").addEventListener('click', confirmUploadDirectories());
 
 // change the visibility of the green image overlay when click the correct button
 document.getElementById("hideGreenImage").addEventListener("click", function () { changeVisibility("greenOverlay", "hideGreenImage")});
 document.getElementById("hideBlueImage").addEventListener("click", function () { changeVisibility("blueOverlay", "hideBlueImage") });
+
 
 // use the slider to change the opacity
 let greenSlider = document.getElementById("greenOpacity");
@@ -98,7 +149,7 @@ greenSlider.oninput = function(){
 let blueSlider = document.getElementById("blueOpacity");
 // change the image opacity each time you drag the slider handler
 blueSlider.oninput = function () {
-    let blue0Overlay = document.getElementById("blueOverlay");
+    let blueOverlay = document.getElementById("blueOverlay");
     blueOverlay.style.opacity = this.value / 100;
 }
 
@@ -106,3 +157,20 @@ blueSlider.oninput = function () {
 // add the callback for the preference
 document.getElementById("betterBlue").addEventListener("click", function(){setChoice(BLUE_BETTER)});
 document.getElementById("betterGreen").addEventListener("click", function () { setChoice(GREEN_BETTER)});
+
+
+// Add keybord shortcut
+
+// KeyBoard shortcuts
+document.addEventListener('keydown', (event) => {
+    switch (event.code) {
+        // If left arrow is pressed
+        case "ArrowLeft":
+            moveImage(-1);
+            break;
+        // If right arrow is pressed
+        case "ArrowRight":
+            moveImage(+1);
+            break;
+    }
+}, false);
